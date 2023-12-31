95a843beb197381772195c596f91a4b019ba74d4
kodewithdky <kodewithdky@gmail.com>
2023-12-31 18:40:16 -0500

    all setup completed

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import morgan from "moran";
+import morgan from "morgan";
 
 //app
 const app = express();
 //middleware
-app.use(morgan("dev"));
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
-);
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
+  );
+  app.use(express.json({ limit: "16kb" }));
+  app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+  app.use(express.static("public"));
+  app.use(cookieParser());
+  app.use(morgan("dev"));
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
