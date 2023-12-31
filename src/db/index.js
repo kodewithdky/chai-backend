3024a43d762dd7cb5a59188039e318ba530cc915
kodewithdky <kodewithdky@gmail.com>
2023-12-31 13:14:57 -0500

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
+        .bgGreen.white
+    );
+  } catch (error) {
+    console.log("MONGODB CONNECTION FAILED".bgRed.white, error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
